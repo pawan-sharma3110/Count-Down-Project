fc552be69659764d424f26690a269b90d2f0e940
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-21 00:25:12 +0530

    basic part ready some javaScript functionalty is left

---
+++ index.js
@@ -0,0 +1,19 @@
+const endDate = "23 March 2024 12:00 AM";
+document.querySelector("#end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+const clock = () => {
+  const currTime = new Date();
+  const endTime = new Date(endDate);
+  const timeDiff = (endTime - currTime) / 1000;
+  if (timeDiff < 0) return;
+  const leftDays = (inputs[0].value = Math.floor(timeDiff / 3600 / 24));
+  const leftHours = (inputs[1].value = Math.floor((timeDiff / 3600) % 24));
+  const leftMintues = (inputs[2].value = Math.floor((timeDiff / 60) % 60));
+  const leftSeconds = (inputs[3].value = Math.floor(timeDiff % 60));
+};
+
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
